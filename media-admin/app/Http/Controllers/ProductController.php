ac95bc96a2020f538fb9783986bf311a911c324f
AllanTran90 <allan_sweden90@hotmail.com>
2026-02-27 11:28:46 +0100

    updated destroy function

--- media-admin/app/Http/Controllers/ProductController.php
+++ media-admin/app/Http/Controllers/ProductController.php
@@ -85,6 +85,7 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        $product->delete();
+        return redirect()->route('products.index');
     }
 }
--- media-admin/resources/views/products/index.blade.php
+++ media-admin/resources/views/products/index.blade.php
@@ -23,6 +23,16 @@
             <td>{{ $product->stock }}</td>
             <td>
                 <a href="{{ route('products.edit', $product) }}">Edit</a>
+                <form action="{{ route('products.destroy', $product) }}"
+                    method="POST"
+                    style="display:inline;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit"
+                            onclick="return confirm('Are you sure?')">
+                        Delete
+                    </button>
+                </form>     
             </td>
         </tr>
     @endforeach
