51a2f3002b59a10a82c7f007454d31eaa1a8646d
Eddie Johansson <eddie.johanssonn@hotmail.com>
2026-03-09 16:45:35 +0100

    Added meta-tag and keyboard navigation css

--- media-admin/resources/views/layouts/app.blade.php
+++ media-admin/resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="description" content="Media admin tool for managing products">
     <title>Media Admin</title>
     @vite(['resources/css/app.css'])
 </head>
